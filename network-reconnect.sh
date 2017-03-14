71a5242b2464a940cc66f76aefacedd4de15dfe1
James Pickard <james.pickard+github@gmail.com>
2017-03-14 10:23:50 +0000

    Try another approach

--- network-reconnect.sh
+++ network-reconnect.sh
@@ -4,17 +4,13 @@ set -e
 ping -c 3 -W 10 8.8.8.8 >/dev/null
 
 if [ $? -ne 0 ]; then
-	echo "$(date) Internet problem detected, restarting network-manager." >> $LOG_FILE
+	echo "$(date) Internet problem detected, restarting network-manager."
 	service networking stop
-	if [[ -f /var/run/wpa_supplicant/wlan0 ]]; then
-		echo "Removing /var/run/wpa_supplicant/wlan0"
-		rm -f /var/run/wpa_supplicant/wlan0
-		wpa_supplicant -B -iwlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
-	fi
-
 	service networking start
+	dhclient -r
+	iwconfig wlan0 essit "Green Dragon Free WiFi"
 	dhclient wlan0
 else
-	echo "$(date) No problem detected." >> $LOG_FILE
+	echo "$(date) No problem detected."
 fi
 
