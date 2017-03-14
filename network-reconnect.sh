ce2b78181ed3090f10777b2b28c64c140f434ebd
James Pickard <james.pickard+github@gmail.com>
2017-03-14 10:12:02 +0000

    Use systemd journal for logging

--- network-reconnect.sh
+++ network-reconnect.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-LOG_FILE=/var/log/network-reconnect/network-reconnect.log
-
-LOG_FILE_SIZE=$(du --bytes $LOG_FILE | awk '{print $1}')
-
-if (( $LOG_FILE_SIZE > 1000000 )); then
-	rm $LOG_FILE
-	echo "$(date) Removed log file, reached 1MB." > $LOG_FILE
-fi
-
 ping -c 3 -W 10 8.8.8.8 >/dev/null
 
 if [ $? -ne 0 ]; then
