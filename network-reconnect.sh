914a3b61bab32aca76a64b105696bacfeb21ce88
James Pickard <james.pickard+github@gmail.com>
2017-03-14 10:11:46 +0000

    Try all the things for reconnecting

--- network-reconnect.sh
+++ network-reconnect.sh
@@ -13,9 +13,11 @@ ping -c 3 -W 10 8.8.8.8 >/dev/null
 
 if [ $? -ne 0 ]; then
 	echo "$(date) Internet problem detected, restarting network-manager." >> $LOG_FILE
-	rm /var/run/wpa_supplicant/wlan0
+	service networking restart
+	rm -f /var/run/wpa_supplicant/wlan0
 	wpa_supplicant -B -iwlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf -Dwext
 	service networking restart
+	dhclient wlan0
 else
 	echo "$(date) No problem detected." >> $LOG_FILE
 fi
